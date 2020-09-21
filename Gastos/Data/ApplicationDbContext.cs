2c6c978f587e9a6ff310d195448f08cc07d8ff6b
Missael Armenta <missael306@gmail.com>
2020-09-21 10:58:12 -0500

    Insertando datos en los catalogos

--- Gastos/Data/ApplicationDbContext.cs
+++ Gastos/Data/ApplicationDbContext.cs
@@ -25,6 +25,90 @@ namespace Gastos.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            #region Create data     
+            Icon entertaiment = new Icon("fas fa-glass-cheers", 1);
+            Icon healt = new Icon("fas fa-hospital", 2);
+            Icon salary = new Icon("fas fa-wallet", 3);
+            Icon car = new Icon("fas fa-car", 4);
+            Icon house = new Icon("fas fa-house-user", 5);
+            Icon food = new Icon("fas fa-utensils", 6);
+            List<Icon> lstIcons = new List<Icon>();
+            lstIcons.Add(entertaiment);
+            lstIcons.Add(healt);
+            lstIcons.Add(salary);
+            lstIcons.Add(car);
+            lstIcons.Add(house);
+            lstIcons.Add(food);
+
+             TypeTransaction outlay = new TypeTransaction();
+            outlay.TypeTransactionID = 1;
+            outlay.Name = "Gasto";     
+
+            TypeTransaction deposit = new TypeTransaction();
+            deposit.TypeTransactionID = 2;
+            deposit.Name = "Ingreso";   
+
+            List<TypeTransaction> lstTypeTransaction = new List<TypeTransaction>();        
+            lstTypeTransaction.Add(outlay);
+            lstTypeTransaction.Add(deposit);
+
+            Category category1 = new Category();
+            category1.CategoryID = 1;
+            category1.Name = "Entretenimiento";           
+            category1.RegisterDate = DateTime.Now;           
+            category1.IconID = entertaiment.IconID;           
+            category1.Active = true; 
+            category1.TypeTransactionID = outlay.TypeTransactionID;
+
+            Category category2 = new Category();
+            category2.CategoryID = 2;
+            category2.Name = "Salud";           
+            category2.RegisterDate = DateTime.Now;           
+            category2.IconID = healt.IconID;           
+            category2.Active = true;   
+            category2.TypeTransactionID = outlay.TypeTransactionID;
+
+            Category category3 = new Category();
+            category3.CategoryID = 3;
+            category3.Name = "Salario";           
+            category3.RegisterDate = DateTime.Now;           
+            category3.IconID = salary.IconID;           
+            category3.Active = true;   
+            category3.TypeTransactionID = deposit.TypeTransactionID;
+
+            Category category4 = new Category();
+            category4.CategoryID = 4;
+            category4.Name = "Automóvil";           
+            category4.RegisterDate = DateTime.Now;           
+            category4.IconID = car.IconID;           
+            category4.Active = true;   
+            category4.TypeTransactionID = outlay.TypeTransactionID;
+
+            Category category5 = new Category();
+            category5.CategoryID = 5;
+            category5.Name = "Casa";           
+            category5.RegisterDate = DateTime.Now;           
+            category5.IconID = house.IconID;           
+            category5.Active = true;   
+            category5.TypeTransactionID = outlay.TypeTransactionID;
+
+            Category category6 = new Category();
+            category6.CategoryID = 6;
+            category6.Name = "Comida";           
+            category6.RegisterDate = DateTime.Now;           
+            category6.IconID = food.IconID;           
+            category6.Active = true;   
+            category6.TypeTransactionID = outlay.TypeTransactionID;
+            
+            List<Category> lstCategories = new List<Category>();
+            lstCategories.Add(category1);
+            lstCategories.Add(category2);
+            lstCategories.Add(category3);
+            lstCategories.Add(category4);
+            lstCategories.Add(category5);
+            lstCategories.Add(category6);
+            #endregion
+
             #region extra configurations            
             modelBuilder.Entity<Transaction>().HasOne(t => t.TypeTransaction).WithMany(x => x.Transactions).OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<Transaction>().Property(t => t.Value).HasColumnType("decimal(18,2)");            
@@ -35,7 +119,12 @@ namespace Gastos.Data
 
             modelBuilder.Entity<Category>().Property(c => c.Active).HasDefaultValue(true);
             modelBuilder.Entity<Category>().Property(c => c.RegisterDate).HasDefaultValueSql("GETDATE()");
+            #endregion
 
+            #region set the tables            
+            modelBuilder.Entity<Icon>().HasData(lstIcons);            
+            modelBuilder.Entity<TypeTransaction>().HasData(lstTypeTransaction);
+            modelBuilder.Entity<Category>().HasData(lstCategories);            
             #endregion
         }
     }
---
+++ Gastos/Data/Migrations/20200921155610_InsertDataCatalogs.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gastos.Data.Migrations
+{
+    public partial class InsertDataCatalogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TypeTransactions",
+                columns: new[] { "TypeTransactionID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Gasto" },
+                    { 2, "Ingreso" }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Cat",
+                table: "Icons",
+                columns: new[] { "IconID", "IconName" },
+                values: new object[,]
+                {
+                    { 1, "fas fa-glass-cheers" },
+                    { 2, "fas fa-hospital" },
+                    { 3, "fas fa-wallet" },
+                    { 4, "fas fa-car" },
+                    { 5, "fas fa-house-user" },
+                    { 6, "fas fa-utensils" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "Active", "IconID", "Name", "TypeTransactionID" },
+                values: new object[,]
+                {
+                    { 1, true, 1, "Entretenimiento", 1 },
+                    { 2, true, 2, "Salud", 1 },
+                    { 4, true, 4, "Automóvil", 1 },
+                    { 5, true, 5, "Casa", 1 },
+                    { 6, true, 6, "Comida", 1 },
+                    { 3, true, 3, "Salario", 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "TypeTransactions",
+                keyColumn: "TypeTransactionID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TypeTransactions",
+                keyColumn: "TypeTransactionID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                schema: "Cat",
+                table: "Icons",
+                keyColumn: "IconID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                schema: "Cat",
+                table: "Icons",
+                keyColumn: "IconID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                schema: "Cat",
+                table: "Icons",
+                keyColumn: "IconID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                schema: "Cat",
+                table: "Icons",
+                keyColumn: "IconID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                schema: "Cat",
+                table: "Icons",
+                keyColumn: "IconID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                schema: "Cat",
+                table: "Icons",
+                keyColumn: "IconID",
+                keyValue: 6);
+        }
+    }
+}
